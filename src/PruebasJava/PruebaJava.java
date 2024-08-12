8e37329228935d8a3033dd76288e03045a409980
andresnaranjo83 <15254567+andresnaranjo83@users.noreply.github.com>
2024-08-11 21:42:14 -0500

    Update PruebaJava.java
    tercer cambio pero desde GitHub

--- src/PruebasJava/PruebaJava.java
+++ src/PruebasJava/PruebaJava.java
@@ -2,6 +2,7 @@ package PruebasJava;
 public class PruebaJava {
 
 	// Aplicacion fizz buzz
+	// cambio realizado desde github
 	public static void main(String[] args) {
 		for(int i = 1; i <= 100; i++) {
 			if(i % 3 == 0 && i % 5 == 0) {
